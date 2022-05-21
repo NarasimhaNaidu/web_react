68708ab6cd480c6e5643dde9a949f1bf94666803
Sai Nitish <sainitish.arawinz@gmail.com>
2022-05-21 15:44:43 +0530

    color changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 // import { styled } from "@mui/material/styles";
 // import Paper from "@mui/material/Paper";
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { Link } from "react-router-dom";
 
 // const Item = styled(Paper)(({ theme }) => ({
@@ -15,18 +15,21 @@ import { Link } from "react-router-dom";
 export function BodyData() {
   return (
     <Container>
-      <Link to="/signup">Register</Link>&nbsp;&nbsp;&nbsp;&nbsp;
-      <Link to="/">Login</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/form">Form Data</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/card">Cards</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/upload">File Upload</Link>&nbsp;&nbsp;&nbsp;
-      <br />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
-        voluptatem debitis officia laudantium dolorum. Obcaecati, optio placeat
-        blanditiis sed, nam cupiditate est excepturi cum reprehenderit,
-        laudantium debitis reiciendis architecto voluptates?
-      </p>
+      <div className="nayudu">
+        <Link to="/signup">Register</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+        <Link to="/">Login</Link>&nbsp;&nbsp;&nbsp;
+        <Link to="/form">Form Data</Link>&nbsp;&nbsp;&nbsp;
+        <Link to="/card">Cards</Link>&nbsp;&nbsp;&nbsp;
+        <Link to="/upload">File Upload</Link>&nbsp;&nbsp;&nbsp;
+        <Link to="/demo">Practice</Link>&nbsp;&nbsp;&nbsp;
+        <br />
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+          voluptatem debitis officia laudantium dolorum. Obcaecati, optio
+          placeat blanditiis sed, nam cupiditate est excepturi cum
+          reprehenderit, laudantium debitis reiciendis architecto voluptates?
+        </p>
+      </div>{" "}
     </Container>
   );
 }
